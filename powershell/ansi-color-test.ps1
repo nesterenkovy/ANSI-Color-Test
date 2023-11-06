601a2d65e1695f97d59a3354c7bff0f5b7b8969d
Volodymyr Nesterenko <nesterenko.vy@gmail.com>
2023-11-06 17:54:06 +0200

    Added column names (background colors)

--- powershell/ansi-color-test.ps1
+++ powershell/ansi-color-test.ps1
@@ -1,19 +1,40 @@
+# Screen cleaning
+Clear-Host
+
 # Table fields separator
 $separator = " "
 $cellText = " myTest "
 
-# Screen cleaning
-Clear-Host
+# Specify cell length
+$cellLength = 8
 
 # Get ANSI colors 
-$FGcolors = [enum]::GetValues([System.ConsoleColor])
-$BGcolors = $FGcolors
+$listColors = [enum]::GetValues([System.ConsoleColor])
+
+# Move to new line
+Write-Host ""
+
+# Background color name string
+foreach ($bgColor in $listColors) {
+    $colorName = $bgColor.ToString().Replace("Dark", "D_").Replace("Magenta", "Purple")
+    # Right-filled formatting
+    # $formattedHead = $colorName.PadRight($cellLength)
+    # Left-filled formatting
+    # $formattedHead = $colorName.PadLeft($cellLength)
+    # Centering formatting
+    $formattedHead = $colorName.PadLeft(($cellLength + $colorName.Length) / 2).PadRight($cellLength)
+    Write-Host -NoNewline "$separator$formattedHead"
+}
+
+# Move to new line
+Write-Host ""
+
 
 # Cycle to display a table of text and background combinations
-Foreach ($FGcolor in $FGcolors) {
-    Foreach ($BGcolor in $BGcolors) {
-        Write-Host -NoNewline "$cellText" -ForegroundColor $FGcolor -BackgroundColor $BGcolor
+Foreach ($FGcolor in $listColors) {
+    Foreach ($BGcolor in $listColors) {
         Write-Host -NoNewline "$separator"
+        Write-Host -NoNewline "$cellText" -ForegroundColor $FGcolor -BackgroundColor $BGcolor
     }
     Write-Host ""
 }
