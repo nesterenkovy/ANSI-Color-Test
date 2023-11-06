189d51432235a0d324b4b2d332d59b6c36bcdb98
Volodymyr Nesterenko <nesterenko.vy@gmail.com>
2023-11-06 17:02:21 +0200

    New cycle of table generation

--- powershell/ansi-color-test.ps1
+++ powershell/ansi-color-test.ps1
@@ -6,17 +6,9 @@ $colors = @("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagent
     "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")
 
 # Cycle to display a table of text and background combinations
-$index = 0
-for ($row = 0; $row -lt 8; $row++) {
-    for ($col = 0; $col -lt 2; $col++) {
-        $textColor = $colors[$index]
-        $bgColor = "Black"
-        $colorCombo = "$textColor on $bgColor"
-        Write-Host $colorCombo -NoNewline
-        # Displaying a block of characters
-        Write-Host "████████".PadRight(80 - $colorCombo.Length)
-        $index++
-    }
+Foreach ($bgcolor in $colors) {
+    Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
+    Write-Host " on $bgcolor"
 }
 
 # Restoring standard colors
