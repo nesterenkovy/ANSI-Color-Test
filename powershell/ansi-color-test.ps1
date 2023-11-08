9b8f11d1e4bd8907d8ffecf582a9c7d652fe9d87
Volodymyr Nesterenko <nesterenko.vy@gmail.com>
2023-11-08 15:29:53 +0200

    PS script: Window size and function optimization

--- powershell/ansi-color-test.ps1
+++ powershell/ansi-color-test.ps1
@@ -1,66 +1,72 @@
 # Function to get color table
 function Get-ColorTable {
+    [CmdletBinding(SupportsShouldProcess = $false)]
     param (
         [array] $listColors
     )
 
-    # Table title, fields and offset
-    $text = "myTest"
-    $title = "ANSI-16"
-    $offset = " "
+    process {
+        # Table title, fields and offset
+        $text = "myTest"
+        $title = "ANSI-16"
+        $offset = "$($PSStyle.Foreground.Black)|$($PSStyle.Reset)"
 
-    # The ANSI escape sequences
-    $ansiReset = $PSStyle.Reset
-    $ansiReverse = $PSStyle.Reverse
+        # The ANSI escape sequences
+        $ansiReset = $PSStyle.Reset
+        $ansiReverse = $PSStyle.Reverse
 
-    # Calculate the maximum length of color name
-    $cellLength = ($listColors.Name | ForEach-Object { $_.Length } | Measure-Object -Maximum).Maximum
+        # Calculate the maximum length of color name
+        $cellLength = ($listColors.Name | ForEach-Object { $_.Length } | Measure-Object -Maximum).Maximum
 
-    # Formatting title and offset to center
-    $title = $ansiReverse + $title.PadRight($cellLength) + $ansiReset
-    $text = $text.PadLeft(($cellLength + $text.Length) / 2).PadRight($cellLength)
+        # Formatting title and offset to center
+        $title = $ansiReverse + $title.PadRight($cellLength) + $ansiReset
+        $text = $text.PadLeft(($cellLength + $text.Length) / 2).PadRight($cellLength)
 
-    # Get the title of the table in the first line
-    $colorTable = "`n" + $title
-    # Get first line - names of background colors
-    foreach ($columnColor in $listColors) {
-        # Right-filled formatting
-        $colorTable += $offset + $columnColor.Name.PadRight($cellLength)
-    }
-    $colorTable += "`n"
-    
-    # Table rows generation cycles
-    foreach ($fgColors in $listColors) {
-        # Row header - name of the foreground color
-        $colorTable += $fgColors.Name.PadRight($cellLength)
-        $fgColor = $fgColors.Color
-
-        # Generate strings
-        switch ($fgColor) {
-            "Default" { $ansiFG = "" }
-            "Reverse" { $ansiFG = $ansiReverse }
-            default { $ansiFG = $PSStyle.Foreground.($fgColor) }
+        # Get the title of the table in the first line
+        $colorTable = "`n" + $title
+        # Get first line - names of background colors
+        foreach ($columnColor in $listColors) {
+            # Right-filled formatting
+            $colorTable += $offset + $columnColor.Name.PadRight($cellLength)
         }
+        
+        # Table rows generation cycles
+        foreach ($fgColors in $listColors) {
+            $colorTable += "`n"
+            # Row header - name of the foreground color
+            $colorTable += $fgColors.Name.PadRight($cellLength)
+            $fgColor = $fgColors.Color
 
-        # Cycle of generating cells in a row
-        foreach ($bgColors in $listColors) {
-            $bgColor = $bgColors.Color
-            switch ($bgColor) {
-                "Default" { $ansiBG = "" }
-                "Reverse" { $ansiBG = $ansiReverse }
-                default { $ansiBG = $PSStyle.Background.($bgColor) }
-            }            
-            $colorTable += $offset + $ansiFG + $ansiBG + $text + $ansiReset
+            # Generate strings
+            switch ($fgColor) {
+                "Default" { $ansiFG = "" }
+                "Reverse" { $ansiFG = $ansiReverse }
+                default { $ansiFG = $PSStyle.Foreground.($fgColor) }
+            }
+
+            # Cycle of generating cells in a row
+            foreach ($bgColors in $listColors) {
+                $bgColor = $bgColors.Color
+                switch ($bgColor) {
+                    "Default" { $ansiBG = "" }
+                    "Reverse" { $ansiBG = $ansiReverse }
+                    default { $ansiBG = $PSStyle.Background.($bgColor) }
+                }            
+                $colorTable += $offset + $ansiFG + $ansiBG + $text + $ansiReset
+            }
         }
-        # End line
-        $colorTable += "`n"
+        # Return the resulting color table
+        $colorTable
     }
-    # Return the resulting color table
-    $colorTable
 }
 
 # Screen cleaning
 Clear-Host
+
+# Get and display terminal window size
+$windowWidth = $host.UI.RawUI.WindowSize.Width
+$windowHeight = $host.UI.RawUI.WindowSize.Height
+Write-Host "WindowSize: $windowWidth x $windowHeight"
 
 # Array of color names and values of $PSStyle
 $listColors = @(
